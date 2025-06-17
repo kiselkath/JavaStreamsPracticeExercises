36ca18dc82e671247409518c4c7c068582dcddb9
katekiselevich <kiselkath@gmail.com>
2025-06-17 17:59:06 +0200

    Exercise 2: Group Products by Category

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -24,5 +25,29 @@ public class Demo
 
         /// 📦 Exercise 2: Group Products by Category
 
+        List<Product> products = List.of(
+                new Product("ivy-oak-tilda", "tops", 85.0),
+                new Product("adidas-originals", "tops", 44.95),
+                new Product("next-regular-fit", "blouses", 37.0),
+                new Product("stradivarius-flowing-palazzo", "trousers", 25.99),
+                new Product("adidas-sportswear", "trousers", 69.95),
+                new Product("be-sunset-lola", "dresses", 227.50),
+                new Product("mango-maxi-dress", "dresses", 79.99),
+                new Product("cath-kidston-maxi-dress", "dresses", 198.0)
+        );
+
+        System.out.println("\uD83D\uDCE6 Exercise 2: Group Products by Category");
+
+        /// цепочка stream-операций или Stream API pipeline
+        Map<String, List<String>> groupedByCategory = products.stream()
+                .collect(Collectors.groupingBy(
+                        Product::getCategory,
+                        Collectors.mapping(Product::getName, Collectors.toList())
+                ));
+
+        groupedByCategory.forEach((category, productNames) -> {
+            System.out.println("Category: " + category);
+            productNames.forEach(name -> System.out.println("  • " + name));
+        });
     }
-}
+;}
